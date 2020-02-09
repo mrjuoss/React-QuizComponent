d538412f60fe6a76057bd02d8a3190d6f6afa864
Jaki <mrjuoss@gmail.com>
2020-02-09 18:46:38 +0700

    Fix Bug

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 
 class QuizEnd extends Component {
+  
   handleResetClick() {
-    this.props.handleResetClick()
+    this.props.resetClickHandler()
   }
   render() {
     return (
